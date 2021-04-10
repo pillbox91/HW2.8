d3341b4839e218c93bbfca0888f58d13a3156dc9
Andrey Averyanov <averyanov1996@gmail.com>
2021-04-10 20:25:47 +0300

    Initial Commit

---
+++ HW2.8/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.8
+//
+//  Created by Андрей Аверьянов on 10.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
